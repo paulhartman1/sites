aa4fb1e18afd1d3ce882d36fa83c3902dffb0df5
Paul Hartman <phartman@itscovered.com>
2023-07-22 11:22:21 -0600

    hide OOO

--- src/app/4/page.tsx
+++ src/app/4/page.tsx
@@ -5,14 +5,11 @@ import { UserProvider } from '@auth0/nextjs-auth0/client';
 import { Event as EventType } from '../components/event/types';
 import { google } from 'googleapis';
 
-
-
 const getEvents = async () => {
   const auth = new google.auth.GoogleAuth({
-    credentials:{
+    credentials: {
       client_email: process.env.GOOGLE_CALENDAR_CLIENT_EMAIL,
       private_key: process.env.GOOGLE_CALENDAR_PRIVATE_KEY,
-     
     },
     scopes: [
       // set the right scope
@@ -26,20 +23,27 @@ const getEvents = async () => {
     calendarId: process.env.GOOGLE_CALENDAR_ID,
   });
 
-
-  const rtn: EventType[] = [];
+  let rtn: EventType[] = [];
   json.data.items!.forEach((item: any) => {
     rtn.push({
-      date: new Intl.DateTimeFormat("en-US").format(new Date(item.start!.dateTime)),
+      date: new Intl.DateTimeFormat('en-US').format(
+        new Date(item.start!.dateTime)
+      ),
       title: item.summary || 'Private Event',
       location: item.location || '',
-      time: `${new Date(item.start!.dateTime).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})} - ${new Date(item.end!.dateTime).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}`,
+      time: `${new Date(item.start!.dateTime).toLocaleTimeString([], {
+        hour: '2-digit',
+        minute: '2-digit',
+      })} - ${new Date(item.end!.dateTime).toLocaleTimeString([], {
+        hour: '2-digit',
+        minute: '2-digit',
+      })}`,
       description: item.description || '',
     });
-  }
- 
-  );
- 
+  });
+  rtn = rtn.filter((event) => {
+    return event.title.toLowerCase() != 'out of office';
+  });
 
   return rtn.sort((a, b) => {
     return new Date(a.date).getTime() - new Date(b.date).getTime();
