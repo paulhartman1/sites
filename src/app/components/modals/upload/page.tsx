c84440c529e43a51da535c2d80a72158ffb6f66a
Paul Hartman <phartman@itscovered.com>
2023-07-23 21:05:47 -0600

    add use client

--- src/app/components/modals/upload/page.tsx
+++ src/app/components/modals/upload/page.tsx
@@ -1,5 +1,5 @@
 
-
+'use client';
 import { Grid, Dropdown, Modal } from '@nextui-org/react';
 import React, { useEffect, useState } from 'react';
 import { UserProvider } from '@auth0/nextjs-auth0/client';
